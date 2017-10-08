5b8ed236619b6e2dafbecbb881462259e3fa4b07
Quirk <>
2017-10-08 21:44:07 +0100

    1.3.0 -> 1.3.1 savefile version update

--- Sil/src/defines.h
+++ Sil/src/defines.h
@@ -58,7 +58,7 @@
  */
 #define VERSION_MAJOR	1
 #define VERSION_MINOR	3
-#define VERSION_PATCH	0
+#define VERSION_PATCH	1
 #define VERSION_EXTRA	0
 
 
@@ -67,7 +67,7 @@
  */
 #define OLD_VERSION_MAJOR	1
 #define OLD_VERSION_MINOR	3
-#define OLD_VERSION_PATCH	0
+#define OLD_VERSION_PATCH	1
 
 
 /*
