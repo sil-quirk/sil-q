700d737c7b6b7d5f449238744014d035616ede3e
Quirk <>
2018-04-10 23:00:32 +0100

    Song of Valour's calculation changed to be more in line with other checks.

--- src/cmd1.c
+++ src/cmd1.c
@@ -3949,7 +3949,7 @@ int py_attack_aux(int y, int x, int attack_type)
 				{
 					knocked = knock_back(p_ptr->py, p_ptr->px, y, x);
  				}
-				if (singing(SNG_VALOUR) && dieroll(ability_bonus(S_SNG, SNG_VALOUR)) > monster_skill(m_ptr, S_WIL) && !(r_ptr->flags2 & RF2_MINDLESS))
+				if (singing(SNG_VALOUR) && skill_check(PLAYER, ability_bonus(S_SNG, SNG_VALOUR), monster_skill(m_ptr, S_WIL), m_ptr) > 0 && !(r_ptr->flags2 & RF2_MINDLESS))
 				{
 					if (r_ptr->flags3 & RF3_NO_STUN)
 					{
@@ -3960,7 +3960,7 @@ int py_attack_aux(int y, int x, int attack_type)
 					}
 					else
 					{
-						stun_monster(m_ptr, ability_bonus(S_SNG, SNG_VALOUR) / 2);
+						stun_monster(m_ptr, ability_bonus(S_SNG, SNG_VALOUR) / 4);
 						msg_format("Your mighty blow stuns %s!", m_name);
 					}
 				}
