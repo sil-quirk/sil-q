bc46e323b258f6da98b5ec493274d33cb2e123e3
Quirk <>
2018-12-18 12:32:34 +0000

    Wrath now keeps no-fear monsters aggressive instead of confident. Double wrath doesn't cancel.

--- src/melee2.c
+++ src/melee2.c
@@ -5260,7 +5260,11 @@ void calc_stance(monster_type *m_ptr)
 	// Some monsters are immune to (non-magical) fear
 	if ((r_ptr->flags3 & (RF3_NO_FEAR)) && (m_ptr->tmp_morale >= 0))
 	{
-		stances[0] = STANCE_CONFIDENT;
+		// Wrath means all aggro all the time
+		if (p_ptr->aggravate && !(r_ptr->flags2 & (RF2_MINDLESS)))
+			stances[0] = STANCE_AGGRESSIVE;
+		else
+			stances[0] = STANCE_CONFIDENT;
 	}
 
 	// Song of Challenge makes monsters attack overconfidently
@@ -5283,7 +5287,7 @@ void calc_stance(monster_type *m_ptr)
 	}
 	
 	// aggravation makes non-mindless things much more hostile
-	if (p_ptr->aggravate & !(r_ptr->flags2 & (RF2_MINDLESS)))
+	if (p_ptr->aggravate && !(r_ptr->flags2 & (RF2_MINDLESS)))
 	{
 		stances[1] = STANCE_AGGRESSIVE;
 	}
