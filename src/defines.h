57156dcf45c5a66def81e748df771fdbcd66164a
Quirk <>
2019-12-31 23:34:58 +0000

    Version now 1.4.3-beta.

--- src/defines.h
+++ src/defines.h
@@ -50,7 +50,7 @@
 /*
  * Current version string
  */
-#define VERSION_STRING	"1.4.2"
+#define VERSION_STRING	"1.4.3-beta"
 
 
 /*
@@ -58,7 +58,7 @@
  */
 #define VERSION_MAJOR	1
 #define VERSION_MINOR	4
-#define VERSION_PATCH	2
+#define VERSION_PATCH	3
 #define VERSION_EXTRA	0
 
 
